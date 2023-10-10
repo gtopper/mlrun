ca377a92f304c11cb98d5048defc64beb8a451b6
Gal Topper <galt@iguazio.com>
2023-10-10 17:18:17 +0800

    Home dir

--- server/api/utils/builder.py
+++ server/api/utils/builder.py
@@ -497,7 +497,7 @@ def build_image(
         relative_workdir = runtime.spec.clone_target_dir or ""
         relative_workdir = relative_workdir.removeprefix("./")
 
-        runtime.spec.clone_target_dir = path.join("~", relative_workdir)
+        runtime.spec.clone_target_dir = path.join("$HOME", relative_workdir)
 
     dock = make_dockerfile(
         base_image,
