ae6cb6433cd84e0ee49d26e59ae9072f3ff141a8
Gal Topper <gal.topper@gmail.com>
2025-05-19 11:00:28 +0300

    Update deprecation version

--- server/py/services/api/launcher.py
+++ server/py/services/api/launcher.py
@@ -337,7 +337,7 @@ class ServerSideLauncher(launcher.BaseLauncher):
                         compressed_serving_spec
                     ).decode("utf-8")
                 else:
-                    # TODO: remove in 1.10.0.
+                    # TODO: remove in 1.11.0.
                     if (
                         serving_spec_len >= SERVING_SPEC_MAX_LENGTH / 10
                     ):  # 1MB limitation as it were before the zip
