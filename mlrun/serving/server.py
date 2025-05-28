a3c01e1058fb53f28f190ab2769b3899ac8fabec
Gal Topper <gal.topper@gmail.com>
2025-05-28 17:47:44 +0300

    Add more type annotations

--- mlrun/serving/server.py
+++ mlrun/serving/server.py
@@ -37,6 +37,7 @@ from mlrun.config import config
 from mlrun.errors import err_to_str
 from mlrun.secrets import SecretsStore
 
+from .. import MLClientCtx
 from ..common.helpers import parse_versioned_object_uri
 from ..common.schemas.model_monitoring.constants import FileTargetKind
 from ..datastore import get_stream_pusher
@@ -412,7 +413,7 @@ def v2_serving_init(context, namespace=None):
 
 
 async def async_execute_graph(
-    context,
+    context: MLClientCtx,
     data: DatasetArtifact,
     namespace=None,
 ) -> (list[Any], Any):
@@ -455,7 +456,9 @@ async def async_execute_graph(
     return responses, termination_result
 
 
-def execute_graph(context, data, namespace=None) -> (list[Any], Any):
+def execute_graph(
+    context: MLClientCtx, data: DatasetArtifact, namespace=None
+) -> (list[Any], Any):
     print(f"111 execute_graph(context={context}, data={data}, namespace={namespace})")
     return asyncio.run(async_execute_graph(context, data, namespace=namespace))
 
