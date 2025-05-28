8034f031bd6b0ae4f19d85f7031d0eed6fb328b6
Gal Topper <gal.topper@gmail.com>
2025-05-28 17:54:17 +0300

    Use isinstance

--- mlrun/serving/server.py
+++ mlrun/serving/server.py
@@ -318,7 +318,7 @@ class GraphServer(ModelObj):
     def _process_response(self, context, response, get_body):
         body = response.body
         if (
-            not hasattr(context, "Response")
+            isinstance(context, MLClientCtx)
             or isinstance(body, context.Response)
             or get_body
         ):
