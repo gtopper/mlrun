4025d2be209e4d89fc9364f9d5e923fc2cc0b1fb
Gal Topper <gal.topper@gmail.com>
2025-05-28 14:31:52 +0300

    Fix

--- server/py/services/api/launcher.py
+++ server/py/services/api/launcher.py
@@ -119,7 +119,12 @@ class ServerSideLauncher(launcher.BaseLauncher):
         print(f"111 launch: serving_spec_volume = {serving_spec_volume}")
         print(f"111 launch: runtime = {runtime}")
         if serving_spec_volume is not None and isinstance(runtime, KubejobRuntime):
-            runtime.spec.volumes = runtime.spec.volumes + [serving_spec_volume["volume"]]
+            runtime.spec.volumes = runtime.spec.volumes + [
+                serving_spec_volume["volume"]
+            ]
+            runtime.spec.volume_mounts = runtime.spec.volume_mounts + [
+                serving_spec_volume["volumeMount"]
+            ]
             print(f"111 launch: runtime changed to: {runtime}")
 
         self._validate_runtime(runtime, run)
