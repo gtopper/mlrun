b20bd824ba177affdbb533b374b6affbf0e827da
Gal Topper <gal.topper@gmail.com>
2025-07-08 18:56:57 +0700

    Fix

--- mlrun/serving/server.py
+++ mlrun/serving/server.py
@@ -550,16 +550,16 @@ async def async_execute_graph(
 ) -> list[Any]:
     spec = mlrun.utils.get_serving_spec()
 
-    source_filename = spec.get("filename", None)
     namespace = {}
-    if source_filename:
-        with open(source_filename) as f:
-            exec(f.read(), namespace)
-
     code = os.getenv("MLRUN_EXEC_CODE")
     if code:
         code = base64.b64decode(code).decode("utf-8")
         exec(code, namespace)
+    else:
+        source_filename = spec.get("filename", None)
+        if source_filename:
+            with open(source_filename) as f:
+                exec(f.read(), namespace)
 
     server = GraphServer.from_dict(spec)
 
--- tests/system/runtimes/test_kubejob.py
+++ tests/system/runtimes/test_kubejob.py
@@ -44,7 +44,7 @@ def exec_cli(args, action="run"):
 class TestKubejobRuntime(tests.system.base.TestMLRunSystem):
     project_name = "kubejob-system-test"
 
-    image: str = "artifactory.iguazeng.com:10557/galt/mlrun:1.10.0-rc11-e34adf"
+    image: str = "artifactory.iguazeng.com:10557/galt/mlrun:1.10.0-rc11-85a7b7"
 
     @pytest.mark.smoke
     def test_deploy_function(self):
