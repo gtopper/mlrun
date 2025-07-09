9be0f1406ec700b0b49de92029cef0c6ff3bc185
Gal Topper <gal.topper@gmail.com>
2025-07-09 12:45:56 +0700

    Delete unused code

--- tests/system/runtimes/test_kubejob.py
+++ tests/system/runtimes/test_kubejob.py
@@ -44,7 +44,7 @@ def exec_cli(args, action="run"):
 class TestKubejobRuntime(tests.system.base.TestMLRunSystem):
     project_name = "kubejob-system-test"
 
-    image: str = "artifactory.iguazeng.com:10557/galt/mlrun:1.10.0-rc11-3defcd"
+    image: str = "artifactory.iguazeng.com:10557/galt/mlrun:1.10.0-rc11-64fb21"
 
     @pytest.mark.smoke
     def test_deploy_function(self):
