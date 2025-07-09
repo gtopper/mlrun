0ecaba028c56ea62f899a30e12afc87f63b00045
Gal Topper <gal.topper@gmail.com>
2025-07-09 11:45:37 +0700

    Fix import

--- tests/system/runtimes/test_kubejob.py
+++ tests/system/runtimes/test_kubejob.py
@@ -44,7 +44,7 @@ def exec_cli(args, action="run"):
 class TestKubejobRuntime(tests.system.base.TestMLRunSystem):
     project_name = "kubejob-system-test"
 
-    image: str = "artifactory.iguazeng.com:10557/galt/mlrun:1.10.0-rc11-7e4f0a"
+    image: str = "artifactory.iguazeng.com:10557/galt/mlrun:1.10.0-rc11-186133"
 
     @pytest.mark.smoke
     def test_deploy_function(self):
