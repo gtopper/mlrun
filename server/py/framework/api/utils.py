7632abb01b1fcc6c0a6a7dbf329bc3d2eeb9dbf6
Gal Topper <gal.topper@gmail.com>
2025-07-21 17:03:24 +0700

    Fix

--- server/py/framework/api/utils.py
+++ server/py/framework/api/utils.py
@@ -265,9 +265,11 @@ async def submit_run(
                 serving_spec=json.dumps(serving_spec),
             )
             # update the graph from the function, because MEP IDs were added
-            serving_spec["graph"] = fn.spec.graph.to_dict(strip=True) if fn.spec.graph else {},
             if serving_spec:
                 serving_spec = json.loads(serving_spec)
+                serving_spec["graph"] = (
+                    fn.spec.graph.to_dict(strip=True) if fn.spec.graph else {},
+                )
                 serving_spec["model_endpoint_creation_task_name"] = (
                     model_endpoint_creation_task_name
                 )
