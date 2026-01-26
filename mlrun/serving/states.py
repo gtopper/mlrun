69861f8c1274f62234e39c9a8ed177d1dda372da
Gal Topper <Gal_Topper-BNKK@mckinsey.com>
2026-01-26 21:07:26 +0700

    Implement support for stream responses
    Using the new functionality introduced in storey 1.11.8 / https://github.com/mlrun/storey/pull/605.
    
    [ML-11876](https://iguazio.atlassian.net/browse/ML-11876)

--- mlrun/serving/states.py
+++ mlrun/serving/states.py
@@ -3997,7 +3997,10 @@ def _init_async_objects(context, steps, root):
                 and step.responder
             ):
                 # if responder step (return result), add Complete()
-                step.async_object.to(storey.Complete(full_event=True))
+                # For streaming, use full_event=False so storey yields just the body
+                # For non-streaming, use full_event=True to get the full Event object
+                streaming = getattr(context._server, "streaming", False)
+                step.async_object.to(storey.Complete(full_event=not streaming))
                 wait_for_result = True
 
     source_args = context.get_param("source_args", {})
